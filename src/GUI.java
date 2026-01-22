0f0c1a30b1d3e79f034b0a1f64e9c1dbad2edd44
0xfluke <mertahmet814@gmail.com>
2026-01-22 19:54:03 +0300

    Flow improvements + GUI start

---
+++ src/GUI.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class GUI extends javax.swing.JFrame {
+    public GUI() {
+        JFrame frame = new JFrame();
+        JPanel panel = new JPanel();
+        JButton button1 = new JButton();
+        JButton button2 = new JButton();
+
+    }
+
+
+}
