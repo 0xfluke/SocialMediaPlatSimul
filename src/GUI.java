5293b8b6faa2901a7b19597129479e8caaad0a2a
0xfluke <mertahmet814@gmail.com>
2026-01-29 14:35:52 +0300

    Flow improvements + GUI start

--- src/GUI.java
+++ src/GUI.java
@@ -1,11 +1,23 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class GUI extends javax.swing.JFrame {
     public GUI() {
         JFrame frame = new JFrame();
         JPanel panel = new JPanel();
+        panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
+        panel.setLayout(new GridLayout(0, 1));
+        frame.add(panel,  BorderLayout.CENTER);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("Social Media Platform Simulation");
+        frame.pack();
+        frame.setVisible(true);
+
+
+
         JButton button1 = new JButton();
         JButton button2 = new JButton();
+
 
     }
 
